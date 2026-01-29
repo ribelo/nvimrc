75404b9bffdd53b3cf5e1a9a4286b5d907e49c06
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2026-01-29 13:48:58 +0100

    refactor: align custom plugins with LazyVim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      signcolumn = true,
-      current_line_blame = false,
-    },
-  },
-
-  {
     "NeogitOrg/neogit",
     cmd = "Neogit",
     dependencies = {
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,30 +1,18 @@
 return {
   {
     "sainnhe/gruvbox-material",
-    lazy = false,
+    lazy = true,
     priority = 1000,
-    config = function()
+    init = function()
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_enable_italic = 1
-      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
 
   {
-    "nvim-tree/nvim-web-devicons",
-    lazy = true,
-  },
-
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
+    "LazyVim/LazyVim",
     opts = {
-      options = {
-        theme = "gruvbox-material",
-        globalstatus = true,
-        section_separators = "",
-        component_separators = "│",
-      },
+      colorscheme = "gruvbox-material",
     },
   },
 }
